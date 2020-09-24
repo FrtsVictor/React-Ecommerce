95aa560758c17b00590a96f685d80e59aa10da9b
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-24 08:45:38 -0300

    Component CardAdmin/Style

---
+++ src/components/CardAdmin/sytles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+    display: flex;
+    padding: 15px;
+    margin: 20px;
+    justify-content: space-between;
+
+    & div{
+        flex:1;
+    }
+
+`;
+
+export default StyledCard;
