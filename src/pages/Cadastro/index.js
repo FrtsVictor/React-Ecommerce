f4a33ac28a0b091cdf5caa33afa35fb349403125
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-24 10:13:25 -0300

    Page /cadastro

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormCadastro from '../../components/FormCadastro';
+
+const Cadastro = () => (
+  <>
+    <h1>HEADER</h1>
+    <FormCadastro />
+
+  </>
+);
+
+export default Cadastro;
