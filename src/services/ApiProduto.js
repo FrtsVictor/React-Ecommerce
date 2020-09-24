607f105605f6779325ccfc9932904961a8efc6d7
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-24 18:35:10 -0300

    Rotas CRUD Produto

---
+++ src/services/ApiProduto.js
@@ -0,0 +1,82 @@
+import api from './api';
+
+const apiProduto = {
+
+  loadAll: async () => {
+    try {
+      const response = await api.get('produto');
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+
+  loadByID: async (id) => {
+    try {
+      const response = await api.get(`produto/${id}`);
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+
+  create: async (
+    nome, descricao, qtdEstoque, valor,
+    idCategoria, nomeCategoria, idFuncionario,
+    nomeFuncionario, dataFabricacao, fotoLink,
+  ) => {
+    try {
+      const response = await api.post('produto', {
+        nome,
+        descricao,
+        qtdEstoque,
+        valor,
+        idCategoria,
+        nomeCategoria,
+        idFuncionario,
+        nomeFuncionario,
+        dataFabricacao,
+        fotoLink,
+      });
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+  update: async (
+    id, nome, descricao, qtdEstoque, valor,
+    idCategoria, nomeCategoria, idFuncionario,
+    nomeFuncionario, dataFabricacao, fotoLink,
+  ) => {
+    try {
+      const response = await api.put(`produto${id}`, {
+        nome,
+        descricao,
+        qtdEstoque,
+        valor,
+        idCategoria,
+        nomeCategoria,
+        idFuncionario,
+        nomeFuncionario,
+        dataFabricacao,
+        fotoLink,
+      });
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+
+  delete: async (id) => {
+    try {
+      const response = await api.delete(`produto/${id}`);
+      return response.data;
+    } catch (error) {
+      return console.log(error);
+    }
+  },
+
+};
+
+export default apiProduto;
