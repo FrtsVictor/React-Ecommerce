625914ad9f217ddf5057eed0fa73d5564c3ea973
Lais <lais-mm@outlook.com>
2020-09-25 12:43:38 -0300

    Add page carrinho

---
+++ src/components/FormCadastro/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledCadastro = styled.div`
+    margin: 120px;
+    font-size: 16px;
+
+    #button{
+        background-color: #6D3CAB;
+        color: #fff;
+    }
+
+    #button:hover{
+        background-color: #401F6B;
+    }
+`;
+
+export default StyledCadastro;
---
+++ src/pages/Carrinho/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../components/Nav';
+import Footer from '../../components/Footer'
+const Carrinho = () => (
+<>
+<Nav/>
+<h1>carrinho</h1>
+<Footer/>
+</>
+)
+
+export default Carrinho;
