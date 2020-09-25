e095d73dc30d1bf30d9a8d3171bb3a4cc63ecb1b
Luana Santos <saluana@outlook.com>
2020-09-25 02:25:02 -0300

    imagem para produto sem foto

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,12 +7,12 @@ const Footer = () => (
   <StyledFooter>
 
     <div className="flex-container">
-      <img className="img" src={logo}></img>
+      <img className="img" src={logo} alt="logo"></img>
       <p className="texto"><em>Desenvolvido por TechTeam \o/</em></p>
       <div className="icones">   
-        <a href="#" className="fa fa-instagram"></a>
-        <a href="#" className="fa fa-facebook"></a>
-        <a href="#" className="fa fa-twitter"></a>
+        <a href="https://instagram.com" className="fa fa-instagram" target="_blank"></a>
+        <a href="https//facebook.com" className="fa fa-facebook" target="_blank"></a>
+        <a href="https://twitter.com" className="fa fa-twitter" target="_blank"></a>
       </div>    
     </div>
 
