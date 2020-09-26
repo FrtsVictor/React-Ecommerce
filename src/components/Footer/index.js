db41b910ad152b68e741ebca3972711edebdfe84
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-26 20:16:45 -0300

    Identando Codigo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
-import {StyledFooter} from './styles';
+import { StyledFooter } from './styles';
 import logo from '../../assets/imgs/site-seguro.png';
 
 const Footer = () => (
@@ -7,13 +10,13 @@ const Footer = () => (
   <StyledFooter>
 
     <div className="flex-container">
-      <img className="img" src={logo} alt="logo"></img>
+      <img className="img" src={logo} alt="logo" />
       <p className="texto"><em>Desenvolvido por TechTeam \o/</em></p>
-      <div className="icones">   
-        <a href="https://instagram.com" className="fa fa-instagram" target="_blank"></a>
-        <a href="https//facebook.com" className="fa fa-facebook" target="_blank"></a>
-        <a href="https://twitter.com" className="fa fa-twitter" target="_blank"></a>
-      </div>    
+      <div className="icones">
+        <a href="https://instagram.com" className="fa fa-instagram" target="_blank" />
+        <a href="https//facebook.com" className="fa fa-facebook" target="_blank" />
+        <a href="https://twitter.com" className="fa fa-twitter" target="_blank" />
+      </div>
     </div>
 
   </StyledFooter>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import { Navbar } from './styles'
+import { Link } from 'react-router-dom';
+import { Navbar } from './styles';
 import logo from '../../assets/imgs/logo.png';
 
-const Nav = ( ) => (
-    <Navbar>
+const Nav = () => (
+  <Navbar>
     <div>
-        <img className="logo" src={logo} alt="logo" width="200" height="100"/> 
+      <img className="logo" src={logo} alt="logo" width="200" height="100" />
+      <ul>
         <ul>
-        <ul>
-        <li>
-          <Link  id="itens" to="/">
-            Home
-          </Link> |  
-        </li>
-        <li>
-          <Link id="itens" to="/produto">
-            Produtos
-          </Link> |  
-        </li>
-        <li>
-          <Link id="itens" to="/admin">
-            Admin
-          </Link>
-        </li>
-      </ul>  
+          <li>
+            <Link id="itens" to="/">
+              Home
+            </Link>
+            {' '}
+            |
+          </li>
+          <li>
+            <Link id="itens" to="/produto">
+              Produtos
+            </Link>
+            {' '}
+            |
+          </li>
+          <li>
+            <Link id="itens" to="/admin">
+              Admin
+            </Link>
+          </li>
         </ul>
+      </ul>
     </div>
-    </Navbar>
+  </Navbar>
 );
 
 export default Nav;
