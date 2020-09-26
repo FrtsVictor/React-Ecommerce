2dcc865faf9bdf896fa9f44440972ebf6c515c62
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-26 20:12:00 -0300

    Adicionando ListContext App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import ListProvider from './services/ListsContext';
 
 function App() {
   return (
 
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <ListProvider>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </ListProvider>
 
   );
 }
