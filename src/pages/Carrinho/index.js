d70317d9c965111ab8cfc393cbab439e3154eb76
Priscila <priscilabarcala@gmail.com>
2020-09-27 20:10:55 -0300

    alterações na pag carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,73 +1,31 @@
 import React from 'react';
-import Nav from '../../components/Nav';
-import Footer from '../../components/Footer'
-import './styles'
-import semFoto from '../../assets/imgs/pdt-sem-imagem.png';
-import { CartProducts, Container, CartFinalizar, Total } from './styles';
-const Carrinho = () => {
+import Footer from '../../components/Footer';
+import { Link } from 'react-router-dom';
+import logobg from '../../assets/imgs/logobg.png'
+import {Container, Logo} from './styles'
+const Bemvindo = () => (
+    <>
+    <Container>
+    <div className="background">
 
-
-const addDefaultImg = (e) => {
-        e.target.src = semFoto;
-      }
-
-return(
-<>
-<Nav/>
-
-<Container>
-<div id="container">
-    <div id="titulo">
-        <p >Minhas compras</p>
+    <Logo>
+    <div className="logo" > 
+    <img src={logobg} alt="logo" width="600" height="200"/> 
     </div>
-    <CartProducts>
-        <div id="box-product">
-            <div className="img">
-                <img src="" onError={addDefaultImg} alt="foto-produto" width="100%"></img>
-            </div>
+    </Logo>
 
-            <div>
-                <p className="nome">Título</p>
-                <p className="descricao">Descrição</p>  
-            </div>
-                
-            <div className="qtd">
-                <label for="quantidade">Qtd</label>
-                <input type="number" name="quantidade" id="quantidade"/>
-            </div> 
-
-            <div className="price">
-                <p>R$0.00</p>        
-            </div> 
-
-            <div className="trash">
-                <i class="far fa-trash-alt fa-border fa-md"/>
-            </div>
-
-        </div>
-    
-        </CartProducts>
-          
-</div>
-             
-</Container>
-        <div>
-        <Total>
-            <div id="total">       
-                    <p>Total</p>
-                    <p>R$0.00</p>  
-            </div>
-        </Total>
-        
-        <CartFinalizar>
-                <div id="btn-finalizar">         
-                    <button> <i class="fas fa-shopping-cart"></i> Ir para o carrinho</button>  
-                </div>
-        </CartFinalizar>
-        </div>
-<Footer/>
-</>
-);
-}
-
-export default Carrinho;
+    <div>
+    <p className="descricao"> Os melhores produtos com os menores preços</p>
+    </div>
+    <div className="btn">
+          <Link  to="/home">
+            <button>Entrar</button>
+          </Link>  
+    </div>
+    </div>  
+    </Container>
+    <Footer/>
+    </>
+  );
+  
+  export default Bemvindo;
--- src/pages/Carrinho/styles.js
+++ src/pages/Carrinho/styles.js
@@ -1,147 +1,54 @@
 import styled from 'styled-components';
+import bg from '../../assets/imgs/bg.jpg'
 
 export const Container = styled.div`
-
-    #container{
-        width: 100vh;
-        height:100vh;
-    }
-    
-    #titulo{
-        display: flex;
-        margin-left: 40px;
-        margin-top: 40px;
-        color: #5a575d;
-        font-family: 'Roboto';
-        font-size:28px;
-        font-weight:600;
-        
-    }
-
-    
-}
-
-
-`;
-
-export const CartProducts = styled.div`
-
-    #box-product{
-        display: flex;
-        justify-content:space-between;
-        width:1200px;
-        height: 100px;
-        background-color: #bdbdbe;
-        margin-left: 40px;
-        margin-top: 60px;
-    }
-
-    img{
-        width: 90px;
-        height: 90px;
-        justify-content: flex-start;
-        margin: 5px 0 5px 10px;
-    }
-
-    .trash{
-        justify-content:flex-end;
-        color:#3b393d; 
-        margin-right: 10px;
-        margin-top: 5px;
-        cursor: pointer;
-    }
-
-    .nome{
-        color:#3b393d;
-        font-size: 18px;
-        font-weight: 600;
-        margin-left: -180px;
-        margin-top: 10px;
+    .background{
+        background-image: url(${bg});
+        height: 100vh;
+        width: 100vw;
     }
 
     .descricao{
-        color:#3b393d;
-        font-size: 14px;
-        font-weight: 400;
-        margin-left: -180px;
-        margin-top: 10px;
-        max-width: 50ch;
-    }
-
-    .qtd{
-        display: flex;
-        color:#3b393d; 
-        margin-left: 100px;
-        margin-top: 30px;
-    }
-    .qtd input{
-        width: 50px;
-        height: 30px;
-        padding-left: 10px;
-        border: none;
-        border-bottom: 1px solid #333;
-    }
-
-    .qtd label{
-        color:#3b393d;
-        margin-right: 10px;
-        margin-top: 5px;
+        text-align: center;
+        color: #d6d4d7;
+        font-size: 28px;
+        margin-top: 40px;
+        font-family: 'Roboto';
         font-weight: 600;
     }
 
-    .price{
-        color:#3b393d; 
-        margin-top: 35px;
-        font-weight: 600;
-        
+    .btn {
+        display:flex;
+        justify-content: center;
+        margin-top: 80px;
     }
 
-`;
-
-export const CartFinalizar = styled.div`
-
- #btn-finalizar {
-     display: flex;
-     justify-content: flex-end;
-     margin-bottom:80px;
-     margin-right: 100px;
-}
-
-
-button{
+    button{
         width: 200px;
         height: 50px;
-        font-size: 16px;
-        font-weight:600;
+        font-size: 24px;
+        font-weight:400;
         font-family: 'Roboto';
         background-color: #6d3cab;
-        color: #fff;
+        color: #d6d4d7;
         border-radius: 5px;
-        border: none;
         transition: background 0.5s;
-        margin-top: 20px;
     }
 
     button:hover{
         background-color: #502784;
     }
 
-    i{
-      margin-right:10px;
-      
-    } 
-
 `;
 
-export const Total = styled.div`
-    
-    #total{
-    display: flex;
-    justify-content: flex-end;
-    margin-bottom: 20px;
-    margin-top: 20px;
-    width:200px;
-    height: 100px;
-    background-color: #bdbdbe;
+    export const Logo = styled.div`
+    .logo{
+        display: flex;
+        justify-content: center;
+        padding-top: 80px;
+        
     }
+
 `;
+
+
