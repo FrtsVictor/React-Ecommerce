0bd419c04628f9113b8487ae498ee37f1fe8cb36
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-28 09:06:56 -0300

    Add Rota Autenticada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import Produto from '../pages/Produto';
 import Admin from '../pages/Admin';
 import Cadastro from '../pages/Cadastro';
-import Carrinho from '../pages/Carrinho'
-import Bemvindo from '../pages/Bemvindo'
+import Carrinho from '../pages/Carrinho';
+import Bemvindo from '../pages/Bemvindo';
+import Login from '../pages/Login';
+import Route from './Route';
 
 const Routes = () => (
   <Switch>
 
-    <Route path="/" component={Bemvindo} exact/>
+    <Route path="/" component={Bemvindo} exact />
+    <Route path="/login" component={Login} />
     <Route path="/home" component={Home} />
-    <Route path="/produto" component={Produto} />
-    <Route path="/admin" component={Admin} />
-    <Route path="/cadastro" component={Cadastro} />
-    <Route path="/carrinho" component={Carrinho} />
+    <Route path="/produto" component={Produto} isPrivate />
+    <Route path="/admin" component={Admin} isPrivate />
+    <Route path="/cadastro" component={Cadastro} isPrivate />
+    <Route path="/carrinho" component={Carrinho} isPrivate />
 
   </Switch>
 
