ce05cb15c6b702a04c10ab52dc24e2ece361afa1
Carlos Elian <elianro03#gmail.com>
2020-09-28 11:15:27 -0300

    Adicinando pages carrinhoVazio/Sucesso

---
+++ src/pages/CarrinhoVazio/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import Nav from '../../components/Nav';
+import TelaCV from '../../components/telaCV';
+
+const CarrinhoVazio = () => (
+    <>
+    <Nav />
+    <TelaCV />
+    <Footer />
+    </>
+  );
+  
+  export default CarrinhoVazio;
---
+++ src/pages/Sucesso/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import Nav from '../../components/Nav';
+import TelaSucesso from '../../components/telaSucesso';
+
+const Sucesso = () => (
+    <>
+    <Nav />
+    <TelaSucesso />
+    <Footer />
+    </>
+  );
+  
+  export default Sucesso;
+  
