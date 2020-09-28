fda811a73f7de609960ec3f3c343d635eee3a3a6
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-28 07:01:21 -0300

    Styles Modal produto

--- src/components/Modal/ModalProduto.js
+++ src/components/Modal/ModalProduto.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import InputModalProduto from '../InputModalProduto';
-import { Button } from '@material-ui/core';
+import Input from '../InputModalProduto';
 
 function getModalStyle() {
   const top = 50;
@@ -19,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
     width: 800,
-    height: 500,
+    height: 400,
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
@@ -29,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleModal() {
   const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
 
@@ -43,15 +41,15 @@ export default function SimpleModal() {
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
-      <InputModalProduto />
+      <Input />
     </div>
   );
 
   return (
     <div>
-      <Button type="button" size="default" color="primary" onClick={handleOpen}>
-          Adicionar
-      </Button> 
+      <button type="button" onClick={handleOpen}>
+        Adicionar Produto
+      </button>
       <Modal
         open={open}
         onClose={handleClose}
