f201e211e53864fd3011a0a1da0a2430f6e4da69
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-28 09:06:19 -0300

    Add AppProvider

---
+++ src/hook/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AuthProvider } from './auth';
+
+const AppProvider = ({ children }) => (
+  <AuthProvider>
+    {children}
+  </AuthProvider>
+);
+
+export default AppProvider;
