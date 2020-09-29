8215164b0c1d3bd857244d28bb6f77c58c9b47c8
Lais <lais-mm@outlook.com>
2020-09-29 07:57:01 -0300

    add clear storage

--- src/components/telaSucesso/index.js
+++ src/components/telaSucesso/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import {StyledSucesso} from './styles';
 import { Link } from 'react-router-dom';
 
-const telaSucesso = () => (
+const telaSucesso = () => {
+    
+    function clearStorage() {
+        localStorage.clear();
+        window.location.href = "/"
+    }
 
+    return (
     <StyledSucesso>
         <div id="divSucesso">
             <i class="far fa-check-circle"></i>
             <p id="texto1">Obrigado!</p>
             <p id="texto2">Compra realizada com sucesso!</p>
-            <Link to="/">
-            <button id="botao">OK</button>
-            </Link>
+            <button onClick={() =>clearStorage()} id="botao">OK</button>
         </div>
   
     </StyledSucesso>
-  
-  );
+    ) 
+    };
   
   export default telaSucesso;
   
