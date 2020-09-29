a58ec106b131e472a82588cde12acf1bd55a441b
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-29 02:32:20 -0300

    Add POST method

--- src/components/InputModalCategoria/index.js
+++ src/components/InputModalCategoria/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
+import apiCategoria from '../../services/apiCategoria';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,20 +13,20 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// BasicTextFields
-
 function InputModalCategoria() {
   const classes = useStyles();
   const [nome, setNome] = useState('');
   const [descricao, setDescricao] = useState('');
 
+  const createCat = () => (
+    {
+      nome,
+      descricao,
+    }
+  );
 
-  const handleFormSubmit = () => [
-    { nome },
-    { descricao },
-  ];
+  const newCat = createCat();
 
-  const newCategoria = handleFormSubmit();
   return (
 
     <form className={classes.root} noValidate autoComplete="off">
@@ -43,14 +44,20 @@ function InputModalCategoria() {
         value={descricao}
         onChange={(e) => { setDescricao(e.target.value); }}
       />
-    
-      <Button 
+
+      <Button
         variant="contained"
         color="primary"
         component="span"
         style={{ width: '50%' }}
         type="button"
-        onClick={() => console.log(newCategoria)}>Adicionar Categoria</Button>
+        onClick={() => apiCategoria.create(newCat)
+          .then((resp) => console.log(resp))
+          .catch((resp) => console.log(resp))}
+      >
+        Adicionar Categoria
+
+      </Button>
     </form>
   );
 }
