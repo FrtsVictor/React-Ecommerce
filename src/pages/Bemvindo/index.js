57d94a3974bfe37458bd4f15f80da25fa830d46e
Lais <lais-mm@outlook.com>
2020-09-29 01:28:51 -0300

    styles

--- src/pages/Bemvindo/index.js
+++ src/pages/Bemvindo/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
-import logobg from '../../assets/imgs/logobg.png';
-import { Container, Logo } from './styles';
-
+import { Link } from 'react-router-dom';
+import logobg from '../../assets/imgs/logobg.png'
+import {Container, Logo} from './styles'
 const Bemvindo = () => (
-  <>
+    <>
     <Container>
-      <div className="background">
+    <div className="background">
 
-        <Logo>
-          <div className="logo">
-            <img src={logobg} alt="logo" width="600" height="200" />
-          </div>
-        </Logo>
+    <Logo>
+    <div className="logo" > 
+    <img src={logobg} alt="logo" width="600" height="200"/> 
+    </div>
+    </Logo>
 
-        <div>
-          <p className="descricao"> Os melhores produtos com os menores preços</p>
-        </div>
-        <div className="btn">
-          <Link to="/home">
+    <div>
+    <p className="descricao"> Os melhores produtos com os menores preços</p>
+    </div>
+    <div className="btn">
+          <Link  to="/login">
             <button>Entrar</button>
-          </Link>
-        </div>
-      </div>
+          </Link>  
+    </div>
+    </div>  
     </Container>
-    <Footer />
-  </>
-);
-
-export default Bemvindo;
+    <Footer/>
+    </>
+  );
+  
+  export default Bemvindo;
