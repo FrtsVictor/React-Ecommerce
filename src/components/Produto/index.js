5199e13377a132f92fb6d54f1fe7dd0a13e45704
FrtsVictor <victorfreitas.job@gmail.com>
2020-09-30 08:03:00 -0300

    Rmv components/Produto

--- src/components/Produto/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Produtos = () =>{
-    return (
-
-    )
-}
-
-export default Produtos;
--- src/components/telaSucesso/index.js
+++ src/components/telaSucesso/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import {StyledSucesso} from './styles';
-import { Link } from 'react-router-dom';
+import { StyledSucesso } from './styles';
 
 const telaSucesso = () => {
-    
-    function clearStorage() {
-        localStorage.clear();
-        window.location.href = "/"
-    }
+  function clearStorage() {
+    localStorage.clear();
+    window.location.href = '/';
+  }
 
-    return (
+  return (
     <StyledSucesso>
-        <div id="divSucesso">
-            <i class="far fa-check-circle"></i>
-            <p id="texto1">Obrigado!</p>
-            <p id="texto2">Compra realizada com sucesso!</p>
-            <button onClick={() =>clearStorage()} id="botao">OK</button>
-        </div>
-  
-    </StyledSucesso>
-    ) 
-    };
-  
-  export default telaSucesso;
-  
+      <div id="divSucesso">
+        <i className="far fa-check-circle" />
+        <p id="texto1">Obrigado!</p>
+        <p id="texto2">Compra realizada com sucesso!</p>
+        <button type="button" onClick={() => clearStorage()} id="botao">OK</button>
+      </div>
 
+    </StyledSucesso>
+  );
+};
+
+export default telaSucesso;
